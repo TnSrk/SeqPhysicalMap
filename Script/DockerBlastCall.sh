1fc24100c7b872f134b09fbdd70d3948070bb143
TnSrk <tanawut.cool@gmail.com>
2025-10-02 17:31:33 +0700

    Update DockerBlastCall.sh

--- Script/DockerBlastCall.sh
+++ Script/DockerBlastCall.sh
@@ -1,2 +1,2 @@
 echo > /wD/blastn_out.tab;
-blastn -num_threads 4 -query "$1" -db /wD/BLASTDB/RotaVDB -max_hsps 1 -perc_identity 97.0 -evalue 1e-50 -outfmt 6 | sort -k12,12gr | sort -u -k2,2 | awk -v N="" '{if ($2 != N) {print $0;N=$2} }' | head -n 500 | awk '{print "echo -e \""$0"\" >> /wD/blastn_out.tab;bash /wD/Script/IdGrep.sh "$2}'  | bash 
+blastn -num_threads 4 -query "$1" -db /wD/BLASTDB/VDB -max_hsps 1 -perc_identity 97.0 -evalue 1e-50 -outfmt 6 | sort -k12,12gr | sort -u -k2,2 | awk -v N="" '{if ($2 != N) {print $0;N=$2} }' | head -n 500 | awk '{print "echo -e \""$0"\" >> /wD/blastn_out.tab;bash /wD/Script/IdGrep.sh "$2}'  | bash 
